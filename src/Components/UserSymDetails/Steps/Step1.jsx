bd5de63b85d49337f17206da8cd82edd461ebd89
immanent-vishal <144302271+immanent-vishal@users.noreply.github.com>
2023-09-07 09:00:28 +0530

    Add files via upload

---
+++ src/Components/UserSymDetails/Steps/Step1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Step1() {
+
+
+    
+
+
+  return (
+   <>
+    <div>
+            <div>
+            <h2></h2>
+            </div>
+    </div>
+   </>
+  )
+}
